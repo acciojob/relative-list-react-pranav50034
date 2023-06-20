65fe7f6f138e1219de34b04565179eea55fb9232
Pranav Pokale <pranavpokale50034@gmail.com>
2023-06-20 11:51:24 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
+const relativeList = ["relative1","relative2","relative3","relative4","relative5","relative6","relative7"]
+
 const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol key={relativeList}>
+                {
+                  relativeList.map((item, index)=>{
+                      return(
+                        <li key={item}>{item}</li>
+                      )
+                  })
+                }
+               </ol>
     </div>
   )
 }
